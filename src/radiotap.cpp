e590b16c9d831a1ff4b6475fc84c5a1af71dd1ef
eden barby <eden.barby@outlook.com>
2016-12-28 13:07:56 +1000

    Added radiotap.cpp and .h to handle radiotap parsing.

---
+++ src/radiotap.cpp
@@ -0,0 +1,51 @@
+/*
+********************************************************************************
+** @file   repo/wep-cracker/src/radiotap.cpp
+** @author eden barby
+** @date   28th december 2016
+** @brief  
+********************************************************************************
+** external functions
+********************************************************************************
+** 
+********************************************************************************
+*/
+
+
+/* includes *******************************************************************/
+
+#include "radiotap.h"
+
+/* private typedefs ***********************************************************/
+/* private defines ************************************************************/
+/* private macros *************************************************************/
+/* private variables **********************************************************/
+/* private function prototypes ************************************************/
+
+
+Radiotap::RatioTap(void)
+{
+}
+
+int Radiotap::init(uint8_t *frame)
+{
+    if((frame[0] != 0) || (frame[1] != 0)) {
+        return 1;
+    }
+
+    this.version_ = frame[0];
+    this.length_  = le16toh((uint16_t)frame[2]);
+    this.present_flags_.push_back(le32toh((uint32_t)frame[4]));
+
+    if(this.length_ < 8) {
+        return 1;
+    }
+
+    int i = 0;
+    while((this.present_flags_[i] >> 31) & 1) {
+        i++;
+        this.present_flags_.push_back(le32toh((uint32_t)frame[4*i + 4]));
+    }
+
+    
+}
